95369e0758a2bd9bd10e4539c2bf84c96efe2e9d
Jesus Suarez <jesus_suarez_11@outlook.com>
2023-12-13 14:29:34 -0500

    estructura inicial terminada, falta aplicar vue

--- javascript/characters.js
+++ javascript/characters.js
@@ -1,3 +1,4 @@
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 let url = new URLSearchParams(window.location.search)
 let searchParams = url.get('page')
 let characterSchema = {
@@ -44,26 +45,47 @@ if (Object.keys(filters).length > 0) {
 fetch(apiUrl)
     .then(response => response.json())
     .then(responseData => {
-        data = responseData
-        let characterCards = document.getElementById("characterCards")
-        for (let i = 0; i < data.results.length; i++) {
-            let card = document.createElement("div")
-            card.classList.add("col-12", "col-lg-4", "my-2")
-            card.innerHTML = `
-                <div class="card">
-                    <img src="${data.results[i].image}" class="card-img-top" onerror="this.src="images/default.png"">
-                    <div class="card-body">
-                        <h3 class="card-title mb-4 text-center">${data.results[i].name}</h3>
-                        <p class="card-text">Origin: ${data.results[i].origin.name}</p>
-                        <p class="card-text">Specie: ${data.results[i].species}</p>
-                        <p class="card-text">Gender: ${data.results[i].gender}</p>
-                        <p class="card-text">Status: ${data.results[i].status}</p>
+        let data = responseData;
+        //---------------------------------- Elements Render ------------------------------------
+
+        // Main cards Render
+        function renderCharacterCards(data, favorites) {
+            let characterCards = document.getElementById("characterCards");
+            characterCards.innerHTML = '';
+
+            for (let i = 0; i < data.results.length; i++) {
+                let card = document.createElement("div");
+                card.classList.add("col-12", "col-lg-4", "my-2");
+                card.innerHTML = `
+                    <div class="card">
+                        <p class="bi bi-star-fill position-absolute top-0 end-0 m-3 h1 clickeable" data-value="${data.results[i].id}" id="favoriteButton-${data.results[i].id}"></p>
+                        <img src="${data.results[i].image}" class="card-img-top" onerror="this.src='images/default.png'">
+                        <div class="card-body">
+                            <h3 class="card-title mb-4 text-center">${data.results[i].name}</h3>
+                            <p class="card-text">Origin: ${data.results[i].origin.name}</p>
+                            <p class="card-text">Specie: ${data.results[i].species}</p>
+                            <p class="card-text">Gender: ${data.results[i].gender}</p>
+                            <p class="card-text">Status: ${data.results[i].status}</p>
+                        </div>
                     </div>
-                </div>
-            `
-            characterCards.appendChild(card)
+                `;
+                characterCards.appendChild(card);
+
+                let favoriteButton = document.getElementById(`favoriteButton-${data.results[i].id}`);
+                favoriteButton.addEventListener('click', function () {
+                    favoriteToggle(data.results[i].id);
+                });
+
+                // Verificar si el personaje actual está en la lista de favoritos
+                if (favorites.includes(data.results[i].id.toString())) {
+                    favoriteButton.classList.add('favorite');
+                } else {
+                    favoriteButton.classList.remove('favorite');
+                }
+            }
         }
 
+        // Pagination Render
         let pagination = document.getElementById("pagination")
         let paginationElements = document.createElement("nav")
         paginationElements.setAttribute('aria-label', 'Page navigation example')
@@ -107,7 +129,9 @@ fetch(apiUrl)
             }
         }
 
-        // Filters
+        //---------------------------------- Filters ------------------------------------
+
+        // Sidebar and Offcanvas Category Filter
         let filtersSidebar = document.getElementById('filtersSidebar')
         renderFilter(filtersSidebar)
         let filtersOffcanvas = document.getElementById('filtersOffcanvas')
@@ -180,8 +204,9 @@ fetch(apiUrl)
             window.location.href = urlString
         }
 
+        // Search Filter
         document.getElementById('searchButton').addEventListener('click', filterSearch);
-        document.getElementById('searchInput').addEventListener('keypress', function(event) {
+        document.getElementById('searchInput').addEventListener('keypress', function (event) {
             if (event.key === 'Enter') {
                 event.preventDefault();
                 filterSearch();
@@ -194,5 +219,98 @@ fetch(apiUrl)
             let newUrl = window.location.pathname + '?' + urlParams.toString();
             window.location.href = newUrl;
         }
+
+        //Favorite Filter and Local Storage Save
+        function favoriteToggle(id) {
+            const i = favorites.indexOf(id.toString());
+            if (i !== -1) {
+                favorites.splice(i, 1);
+            } else {
+                favorites.push(id.toString());
+            }
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            console.log("Elementos favoritos:", favorites);
+
+            renderCharacterCards(responseData, favorites);
+            displayFavoriteCharacters();
+
+            const allCharacters = document.querySelectorAll('[id^="favoriteButton-"]');
+            allCharacters.forEach(character => {
+                const characterId = character.getAttribute('data-value');
+                if (favorites.includes(characterId)) {
+                    character.classList.add('favorite');
+                } else {
+                    character.classList.remove('favorite');
+                }
+            });
+        }
+
+        renderCharacterCards(responseData, favorites)
+
     })
 
+async function fetchCharacterData(id) {
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+        if (response.ok) {
+            const characterData = await response.json();
+            return characterData;
+        }
+        throw new Error('Network response was not ok.');
+    } catch (error) {
+        console.error('Error fetching character data:', error);
+    }
+}
+
+
+async function displayFavoriteCharacters() {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    const favoriteCardsContainer = document.getElementById('favoriteCards');
+    favoriteCardsContainer.innerHTML = '';
+    for (const id of favorites) {
+        const characterData = await fetchCharacterData(id);
+
+        if (characterData) {
+
+            const card = document.createElement('div');
+            card.classList.add('col-md-4', 'mb-4');
+            card.innerHTML = `
+                    <div class="card">
+                        <img src="${characterData.image}" class="card-img-top" alt="${characterData.name}">
+                        <div class="card-body">
+                            <h3 class="card-title mb-4 text-center">${characterData.name}</h5>
+                            <p class="card-text">Origin: ${characterData.origin.name}</p>
+                            <p class="card-text">Status: ${characterData.status}</p>
+                            <p class="card-text">Species: ${characterData.species}</p>
+                            <p class="card-text">Gender: ${characterData.gender}</p>
+                        </div>
+                    </div>
+                `;
+            const removeButton = document.createElement('button');
+            removeButton.classList.add('btn', 'btn-danger', 'btn-sm', 'mt-2', 'col-12');
+            removeButton.setAttribute('data-value', id);
+            removeButton.textContent = 'Remove';
+            removeButton.addEventListener('click', function () {
+                removeFavoriteCharacter(id);
+                displayFavoriteCharacters();
+            });
+            card.querySelector('.card-body').appendChild(removeButton);
+            favoriteCardsContainer.appendChild(card);
+
+        }
+    }
+}
+
+function removeFavoriteCharacter(idToRemove) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const indexToRemove = favorites.indexOf(idToRemove.toString());
+    if (indexToRemove !== -1) {
+        favorites.splice(indexToRemove, 1);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayFavoriteCharacters();
+});
